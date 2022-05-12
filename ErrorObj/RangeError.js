2d190f2dc9e7f70ca9fa5148d4f6cb6511f71546
LeThanh <kzxlmdtn888@gmail.com>
2022-05-12 15:58:13 +0700

    ..

---
+++ ErrorObj/RangeError.js
@@ -0,0 +1,26 @@
+function check1(n) {
+    if (!(n >= -500 && n <= 500)) {
+        throw new RangeError("The argument must be between -500 and 500.");
+    }
+}
+try {
+    check1(2000);
+}
+catch (error) {
+    if (error instanceof RangeError) {
+        console.log(error.message);
+    }
+}
+function check(value) {
+    if (["apple", "banana", "carrot"].includes(value) === false) {
+        throw new RangeError('The argument must be an "apple", "banana", or "carrot".');
+    }
+}
+try {
+    check("cabbage");
+}
+catch (error) {
+    if (error instanceof RangeError) {
+        console.log(error.message);
+    }
+}
