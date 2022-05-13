868a524853f971a7e90f85a43da014f4e65247f8
LeThanh <kzxlmdtn888@gmail.com>
2022-05-13 08:45:52 +0700

    ..

---
+++ RangeError/Calculator.js
@@ -0,0 +1,27 @@
+function calculator(number1, number2, operator) {
+    var result;
+    if (operator === '+' && operator === '-' && operator === '*' && operator === '/') {
+        if (operator == '+') {
+            result = number1 + number2;
+        }
+        else if (operator == '-') {
+            result = number1 - number2;
+        }
+        else if (operator == '*') {
+            result = number1 * number2;
+        }
+        else if (operator == '/') {
+            result = number1 / number2;
+        }
+    }
+    else {
+        try {
+            throw new RangeError("The operator must be +,-,*,/");
+        }
+        catch (e) {
+            return e.message;
+        }
+    }
+    return result;
+}
+console.log(calculator(3, 9, '%'));
